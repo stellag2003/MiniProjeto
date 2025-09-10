e5cdf4fba759f32b522940992abec985f7d16235
Stella <stellagmendonca@gmail.com>
2025-09-10 18:02:21 -0300

    1 quase ok, 2 ok, 3 ok

--- inicio.js
+++ inicio.js
@@ -42,28 +42,6 @@ const rl = readline.createInterface({
     }
 
     ];
-    
-    function menuAlunos(){ // essa função cria o menu
-        console.log('......................................................');
-        console.log('....Seja bem-vindo(a) ao Consulta Aluno Grifinória....');
-        console.log('......................................................');
-        console.log('........................--Menu--......................');
-        console.log('...Escolha um número e acesse um de nossos serviços...');
-        console.log('......................................................');
-        console.log('1- Cadastrar novo bruxo-estudante');
-        console.log('2- Relatório de bruxos-estudantes');
-        console.log('3- Buscar bruxo-estudante');
-        console.log('4- Ver média individual de cada bruxo-estudante');
-        console.log('5- Ver média geral da turma');
-        console.log('6- Aluno Destaque da Casa Grifinória');
-        console.log('7- Lista de bruxos-estudantes aprovados');
-        console.log('8- Lista de bruxos-estudantes reprovados');
-        console.log('9- Lista de bruxos-estudantes em recuperação');
-        console.log('0- Encerrar programa mágico');
-        console.log('......................................................');
-        console.log('......................................................');
-        console.log('......................................................');
-    }
 
     function relatorioAlunos(){ // imprime todos os nomes dos alunos 
       console.log('..........Relatório Alunos Grifinória...............');
@@ -78,43 +56,104 @@ const rl = readline.createInterface({
         
     }
 
-    function cadastraAlunos(){ // essa função cadastra, ela pede o nome do aluno, idade e notas
-        rl.question('Por favor, digite o nome do novo bruxinho(a) para o cadastro! ', (resposta) => {
-
-            const alunoNovo = {};
-            alunos.push(alunoNovo);
-            alunoNovo.nome = resposta;
-            console.log(alunos);
+    function buscaAluno(){ // busca o nome do aluno, baseado na resposta que o usuário digitar no terminal
+        rl.question("Digite o nome do bruxo-aluno que deseja pesquisar", nome =>{
             
-             rl.question(' Agora digite a idade do novo bruxinho(a)! ', (idade) =>{
+           if (!/^[A-Za-zÀ-ÿ\s]+$/.test(nome)) { // !/^[A-Za-zÀ-ÿ\s]+$/ é uma expressão usada para verificar se a resposta contem apenas letras
+                console.log('Digite um nome válido');
+                inicio();
+            }
+            else{
+                const alunoEncontrado = alunos.find(aluno =>  aluno.nome.toLowerCase() === nome.toLowerCase());
+ 
+            if(alunoEncontrado)
+            {
+                 console.log(`Aluno encontrado: ${alunoEncontrado.nome}`);
+            }
+            else{
+                console.log('Aluno não encontrado ):');
 
-                    alunoNovo.idade = idade;
-                    //console.log(`Lista de idade atualizada ${idadeAlunos}`);
-                    console.log(alunos);
+            }
 
-                    // for (let i = 0; i < 4; i++) {
+            }
 
-                    //      rl.question(' Agora digite 4 notas do novo bruxinho(a)! ', (nota) =>{
-
-                    //         alunoNovo.notas = [nota][i];
-                               
-                    //     });
-
-                    // }
-                    // console.log(alunoNovo.notas);
-
-                    // alunoNovo.notas = [];
-             
-            });
-    
+            
         });
         
     }
 
+    function cadastraAlunos(){ // essa função cadastra, ela pede o nome do aluno, idade e notas
+        rl.question('Por favor, digite o nome do novo bruxinho(a) para o cadastro! ', (resposta) => {
+
+            if(!/^[A-Za-zÀ-ÿ\s]+$/.test(resposta))
+            {
+                 console.log('Digite um nome válido');
+                 inicio();
+            }
+            else{
+
+                const alunoNovo = {};
+                alunos.push(alunoNovo);
+                alunoNovo.nome = resposta;
+                console.log(alunos);
+                
+                rl.question(' Agora digite a idade do novo bruxinho(a)! ', (idade) =>{
+
+                        alunoNovo.idade = idade;
+                        //console.log(`Lista de idade atualizada ${idadeAlunos}`);
+                        console.log(alunos);
+
+                        // for (let i = 0; i < 4; i++) {
+
+                        //      rl.question(' Agora digite 4 notas do novo bruxinho(a)! ', (nota) =>{
+
+                        //         alunoNovo.notas = [nota][i];
+                                
+                        //     });
+
+                        // }
+                        // console.log(alunoNovo.notas);
+
+                        // alunoNovo.notas = [];
+                
+            });
+    
+
+            }
+
+            
+        });
+        
+    }
+
+    function calculaMediaGeral(){
+        resultado = alunos.reduce(acumulador, atual => atual.notas + acumulador, 0)
+        console.log(resultado);
+    }
+
     function inicio() // essa função chama a menuAlunos() e logo depois pega o número que o usuário digitou
     {
-        menuAlunos();
-        rl.question("Digite o número da opção ", (resposta) =>{
+            console.log('......................................................');
+            console.log('....Seja bem-vindo(a) ao Consulta Aluno Grifinória....');
+            console.log('......................................................');
+            console.log('........................--Menu--......................');
+            console.log('...Escolha um número e acesse um de nossos serviços...');
+            console.log('......................................................');
+            console.log('1- Cadastrar novo bruxo-estudante');
+            console.log('2- Relatório de bruxos-estudantes');
+            console.log('3- Buscar bruxo-estudante');
+            console.log('4- Ver média individual de cada bruxo-estudante');
+            console.log('5- Ver média geral da turma');
+            console.log('6- Aluno Destaque da Casa Grifinória');
+            console.log('7- Lista de bruxos-estudantes aprovados');
+            console.log('8- Lista de bruxos-estudantes reprovados');
+            console.log('9- Lista de bruxos-estudantes em recuperação');
+            console.log('0- Encerrar programa mágico');
+            console.log('......................................................');
+            console.log('......................................................');
+            console.log('......................................................');
+
+            rl.question("Digite o número da opção ", (resposta) =>{
 
             switch (resposta) { // aqui o swith verifica qual o número e executa funções diferentes, de acordo com a resposta
             case '0':
@@ -122,23 +161,46 @@ const rl = readline.createInterface({
                 break;
 
             case '1':
+                console.log("\n")
                 cadastraAlunos();
+                console.log("\n")
                 break;
 
             case '2':
+                console.log("\n")
                 relatorioAlunos();
-                menuAlunos();
+                console.log("\n")
+                break;
+
+            case '3':
+                console.log("\n")
+                buscaAluno();
+                console.log("\n")
+                break;
+        
+            case '5':
+                console.log("\n")
+                calculaMediaGeral();
+                console.log("\n")
                 break;
         
             default:
+                console.log("\n")
+                console.log('Opção inválida, tente novamente!')
+                console.log("\n")
+                
                 break;
-        }
+            }
 
+        // if(resposta !== '0')
+        //     inicio();
 
-        });
+        // });
 
         
-    }
+    });
+
+}
 
     inicio(); // aqui a função é chamada
     
